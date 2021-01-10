e1e7087b42770d1daa119e8a939176307c6f9df8
andre-i <bezwolos@mail.ru>
2021-01-10 21:54:47 +0300

    Make en help

--- app/src/main/java/com/bezwolos/simplets/help/HelpFragment.kt
+++ app/src/main/java/com/bezwolos/simplets/help/HelpFragment.kt
@@ -1,11 +1,13 @@
 package com.bezwolos.simplets.help
 
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -36,6 +38,7 @@ class HelpFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_help, container, false)
+        showText(view.findViewById<TextView>(R.id.help_content))
         // back button action
         view.findViewById<FloatingActionButton>(R.id.fab_back)?.setOnClickListener {
                     Log.d(TAG, "tap on 'fabBack' button ")
@@ -71,4 +74,14 @@ class HelpFragment : Fragment() {
             }
            */
     }
+    /*   =============================  own fun  ========================================= */
+
+    private fun showText( page : TextView){
+        val text = resources.getText(R.string.help_channel).toString()
+        val help = Html.fromHtml(text)
+        page.setText(help)
+    }
+
+
+
 }
